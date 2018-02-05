75ba847e6937b4261edaa1e22e2904bb4770035a
Tino Sambora <krisnasambora@gmail.com>
2018-02-05 18:36:48 +0700

    pattern lib initial setup

---
+++ pattern-lib/lib/elements/Button.js
@@ -0,0 +1,73 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+import * as colors from '../styles/colors';
+
+const buttonSizes = {
+  small: {
+    'font-size': '14px',
+    'line-height': '30px',
+    padding: '0 8px',
+  },
+  medium: {
+    'font-size': '16px',
+    'line-height': '40px',
+    padding: '0 12px',
+  },
+  large: {
+    'font-size': '18px',
+    'line-height': '50px',
+    padding: '0 16px',
+  },
+  wide: {
+    'font-size': '16px',
+    'line-height': '40px',
+    padding: '0 36px',
+  },
+  extraWide: {
+    'font-size': '16px',
+    'line-height': '40px',
+    padding: '0 72px',
+  },
+  fullWidth: {
+    'font-size': '16px',
+    'line-height': '40px',
+    padding: '0 8px',
+  },
+};
+
+function setDisplay({ size }) {
+  return size === 'fullWidth' ? 'block' : 'inline-block';
+}
+function setWidth({ size }) {
+  return size === 'fullWidth' ? '100%' : 'initial';
+}
+
+const StyledButton = styled.button`
+  background: ${({ bgColor }) => colors[bgColor]};
+  border: none;
+  border-radius: 2px;
+  color: ${({ fontColor })  => colors[fontColor]};
+  cursor: pointer;
+  display: ${setDisplay};
+  font-size: ${({ size }) => buttonSizes[size]['font-size']};
+  line-height: ${({ size }) => buttonSizes[size]['line-height']};
+  font-weight: 200;
+  margin: 8px 0;
+  outline: none;
+  padding: ${({ size }) => buttonSizes[size]['padding']};
+  width: ${setWidth};
+  text-transform: uppercase;
+  transition: all 300ms ease;
+  &:hover {
+    background: ${({ bgColor })  => darken(0.1, colors[bgColor])};
+  }
+`;
+
+StyledButton.defaultProps = {
+  bgColor: 'blue',
+  fontColor: 'white',
+  size: 'medium',
+};
+
+export default StyledButton;
---
+++ pattern-lib/lib/index.js
@@ -0,0 +1,7 @@
+import Button from './elements/Button';
+import Label from './elements/Label';
+
+module.exports = {
+  Button,
+  Label,
+};
